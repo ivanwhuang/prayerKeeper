bed375ece9c5cf1fa1ecb66ece5dbc063ecd5df4
Ivan Huang <huangiw@uci.edu>
2019-09-20 12:08:56 -0700

    Added React and client side dependencies

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment className='App'>
+      <h1>App</h1>
+    </Fragment>
+  );
+};
+
+export default App;
