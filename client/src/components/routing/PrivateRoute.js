65e62e8f9fa5879ca4446fe4080d9f411bb4aa88
Ivan Huang <huangiw@uci.edu>
2019-09-21 16:29:11 -0700

    Added Private Route component

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  // ...rest destructures the rest
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    ></Route>
+  );
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
